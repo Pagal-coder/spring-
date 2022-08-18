d52e2a8b13f2e9f4d6736ec066cec908ed39bf81
pankaj <pankaj19nit@gmail.com>
2022-08-18 07:47:16 +0530

    First

---
+++ src/main/java/com/onlyjavatech/pankaj/SpringbootprojectApplication.java
@@ -0,0 +1,14 @@
+package com.onlyjavatech.pankaj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootprojectApplication.class, args);
+		System.out.println("pankaj is my name");
+	}
+
+}
